efaeedbbaeb8409ecc7ad57782ace3bdf33c6a95
Andrew Ahn <aahn@thrillist.com>
2014-01-17 17:24:43 -0500

    Initial audite test files

---
+++ playsound.rb
@@ -0,0 +1,18 @@
+require 'audite'
+
+player = Audite.new
+
+player.events.on(:complete) do
+    puts "COMPLETE"
+end
+
+player.events.on(:position_change) do |pos|
+    puts "POSITION: #{pos} seconds  level #{player.level}"
+end
+
+puts "Load song"
+
+player.load('budligt_real_chinese.mp3')
+player.toggle
+player.start_thread
+
