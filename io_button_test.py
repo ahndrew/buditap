e455636367b0d21c27f7f561aeb664117b45c8c9
Jimmy Lee <jlee@thrillist.com>
2014-01-31 16:23:43 -0500

    more change

--- io_button_test.py
+++ io_button_test.py
@@ -1,12 +1,13 @@
 #io_button_test
 import RPi.GPIO as GPIO
 import time
-
+#GPIO.setmode(BCM)
 #adjust for where your switch is connected
-buttonPin = 24
+#buttonPin = 24
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(buttonPin,GPIO.IN)
+GPIO.setup(24,GPIO.IN)
+input = GPIO.input(24)
 
 while True:
-  if (GPIO.input(17)):
+  if (GPIO.input(24)):
     print("Button Pressed")
