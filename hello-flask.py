1b5e8e33462a4ca30c89697f556710ebd75d9cea
Francisco Gutierrez <fgutierrez@thrillist.com>
2014-02-05 16:40:17 -0500

    create front-end index template, added bigvideo.js

--- hello-flask.py
+++ hello-flask.py
@@ -1,9 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for, abort, session
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello Francisco!"
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/on')
+def turnon():
+  return "tap on!"
+# load readtilt.py
+
+@app.route('/off')
+def turnoff():
+  return "tap off!"
+# kill readtilt.py
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80, debug=True)
+    app.debug = True
+    app.run()
+    #app.run(host='0.0.0.0', port=, debug=True)
