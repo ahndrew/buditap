c81c1b6dfad868d0ca00f0617c65d51c06b105a3
buditap <jim.subtractive+buditap@gmail.com>
2014-05-09 20:00:35 +0000

    Fixed ddl script

--- ddl/21040509-buditapbd.sh
+++ ddl/21040509-buditapbd.sh
@@ -14,7 +14,7 @@ Q2="CREATE TABLE IF NOT EXISTS $tbl_1 (
   id int(12) NOT NULL auto_increment,
   pour_amount varchar(255) NOT NULL,
   user_id int(12),
-  poured_at timestamp(6),
+  poured_at timestamp,
   PRIMARY KEY  (id)
 );"
 
@@ -22,8 +22,8 @@ Q3="CREATE TABLE IF NOT EXISTS $tbl_2 (
   id int(12) NOT NULL auto_increment,
   name varchar(255),
   quantity varchar(255),
-  installed_at timestamp(6),
-  removed_at timestamp(6),
+  installed_at timestamp,
+  removed_at timestamp,
   PRIMARY KEY  (id)
 );"
 
@@ -31,10 +31,10 @@ Q4="CREATE TABLE IF NOT EXISTS $tbl_3 (
   id int(12) NOT NULL auto_increment,
   proximity varchar(255),
   user_id int(12),
-  timed_at timestamp(6),
+  timed_at timestamp,
   PRIMARY KEY  (id)
 );"
 
 SQL="${Q1}${Q2}${Q3}${Q4}"
 
-mysql -u $username -p$password -e "$SQL" $db_name
+mysql -hec2-54-86-171-52.compute-1.amazonaws.com -u $username -p$password -e "$SQL" $db_name
